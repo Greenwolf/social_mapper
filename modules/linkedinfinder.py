bbb9285a6341e90ad37916666e337847121dec89
Jacob <48361984+Greenwolf@users.noreply.github.com>
2019-05-08 18:26:01 +0100

    Update linkedinfinder.py

--- modules/linkedinfinder.py
+++ modules/linkedinfinder.py
@@ -28,27 +28,45 @@ class Linkedinfinder(object):
 		#agent = self.driver.execute_script("return navigator.userAgent")
 		#print("User Agent: " + agent)
 		
-		if(self.driver.title.encode('ascii','replace').startswith("Sign In")):
+		if(self.driver.title.encode('ascii','replace').startswith("LinkedIn Login")):
 			print("\n[+] LinkedIn Login Page loaded successfully [+]")
 			try:
+				print("debug1")
 				lnkUsername = self.driver.find_element_by_id("session_key-login")
 			except:
-				lnkUsername = self.driver.find_element_by_id("username")
+				try:
+					print("debug2")
+					lnkUsername = self.driver.find_element_by_id("username")
+				except:
+					print("debug3")
+					print("LinkedIn Login Page username field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
+			print("debug4")
 			lnkUsername.send_keys(username)
 			try:
+				print("debug5")
 				lnkPassword = self.driver.find_element_by_id("session_password-login")
 			except:
-				lnkPassword = self.driver.find_element_by_id("password")
+				try:
+					print("debug7")
+					lnkPassword = self.driver.find_element_by_id("password")
+				except:
+					print("debug8")
+					print("LinkedIn Login Page password field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
 			lnkPassword.send_keys(password)
 			try:
 				self.driver.find_element_by_id("btn-primary").click()
 			except:
-				self.driver.find_element_by_class_name("btn__primary--large").click()
+				try:
+					self.driver.find_element_by_class_name("btn__primary--large").click()
+				except:
+					print("LinkedIn Login Page login button seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
 			sleep(5)
 			if(self.driver.title.encode('utf8','replace') == "Sign In to LinkedIn"):
 				print("[-] LinkedIn Login Failed [-]\n")
 			else:
 				print("[+] LinkedIn Login Success [+]\n")
+		else:
+			print("LinkedIn Login Page title field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
 
 
 	def getLinkedinProfiles(self,first_name,last_name,username,password):
@@ -68,11 +86,11 @@ class Linkedinfinder(object):
 		soupParser = BeautifulSoup(searchresponse, 'html.parser')
 		
 
-		# LinkedIn has implemented some code to say no results seemly at random, need code to research if this result pops.
+		#LinkedIn has implemented some code to say no results seemly at random, need code to research if this result pops.
 		## Anti Scraping Bypass (Try 3 times before skipping):
-		# If there are no results do check
+		#If there are no results do check
 		if(len(soupParser.find_all('div', {'class': 'search-result__image-wrapper'})) == 0):
-			# If there is the no results page do an additional try
+			#If there is the no results page do an additional try
 			if(len(soupParser.find_all('div', {'class': 'search-no-results__image-container'}))!=0):
 				#print("First Check")
 				sleep(30)
@@ -112,13 +130,18 @@ class Linkedinfinder(object):
 				#ghostcheck = element.find_all('div')[1]['class']
 
 				link = element.find('a')['href']
+				print(link)
 				#print(link)
-				#profilepic = element.find('img')['src'] - OLD
+				profilepic = element.find('img')['src']
 				#profilepicreplaced = profilepic.replace("/mpr/mpr/shrink_100_100/","/media/") - OLD
-				profilepic = element.find_all('div')[3]['style']
-				profilepicreplaced = profilepic.replace("background-image: url(\"","").replace("\");","")
+				print(profilepic)
+
+				#new then old again
+				#profilepic = element.find_all('div')[3]['style']
+				#profilepicreplaced = profilepic.replace("background-image: url(\"","").replace("\");","")
+
 				#print(profilepicreplaced)
-				picturelist.append(["https://linkedin.com" + link,profilepicreplaced,1.0])
+				picturelist.append(["https://linkedin.com" + link,profilepic,1.0])
 				#print("append successs")
 			#except Exception as e:
 				#print("Error")
