34bdcc89d0f291a8b7bd7e5a2c509aee1efa32c0
Jacob <48361984+Greenwolf@users.noreply.github.com>
2019-06-10 23:26:22 +0100

    Update weibofinder.py

--- modules/weibofinder.py
+++ modules/weibofinder.py
@@ -18,7 +18,11 @@ class Weibofinder(object):
 		display.start()
 		if not showbrowser:
 			os.environ['MOZ_HEADLESS'] = '1'
-		self.driver = webdriver.Firefox()
+		firefoxprofile = webdriver.FirefoxProfile()
+		firefoxprofile.set_preference("permissions.default.desktop-notification", 1)
+		firefoxprofile.set_preference("dom.webnotifications.enabled", 1)
+		firefoxprofile.set_preference("dom.push.enabled", 1)
+		self.driver = webdriver.Firefox(firefox_profile=firefoxprofile)
 
 		self.driver.delete_all_cookies()
 
