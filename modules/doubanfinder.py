35a1be11fce4c131f66da963010d986699bc65ce
Jacob <48361984+Greenwolf@users.noreply.github.com>
2020-09-07 01:22:23 +0100

    Update doubanfinder.py

--- modules/doubanfinder.py
+++ modules/doubanfinder.py
@@ -9,20 +9,27 @@ import json
 import os
 from bs4 import BeautifulSoup
 
+
 class Doubanfinder(object):
 
 	timeout = 10
 	
 	def __init__(self,showbrowser):
-		display = Display(visible=0, size=(1600, 1024))
-		display.start()
+		if sys.platform == "darwin":
+			display = Display(visible=0, size=(1600, 1024))
+			display.start()
+		opts = Options()
 		if not showbrowser:
 			os.environ['MOZ_HEADLESS'] = '1'
+			opts.headless = True
+		else:
+			opts.headless = False
 		firefoxprofile = webdriver.FirefoxProfile()
 		firefoxprofile.set_preference("permissions.default.desktop-notification", 1)
 		firefoxprofile.set_preference("dom.webnotifications.enabled", 1)
 		firefoxprofile.set_preference("dom.push.enabled", 1)
-		self.driver = webdriver.Firefox(firefox_profile=firefoxprofile)
+		self.driver = webdriver.Firefox(firefox_profile=firefoxprofile,options=opts)
+
 		self.driver.implicitly_wait(15)
 		self.driver.delete_all_cookies()
 
@@ -115,5 +122,4 @@ class Doubanfinder(object):
 		except:
 			return ""
 '''
-
 
