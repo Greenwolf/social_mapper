b8673d23af924f70f80c96a4fdc9ca028d2ae8d5
Jacob <48361984+Greenwolf@users.noreply.github.com>
2020-09-09 18:31:10 +0100

    Update pinterestfinder.py

--- modules/pinterestfinder.py
+++ modules/pinterestfinder.py
@@ -68,11 +68,14 @@ class Pinterestfinder(object):
                 sys.exit()
             pinLoginButton.click()
             sleep(5)
-
-            if (self.driver.title.encode('ascii', 'replace').startswith(bytes("Pinterest", 'utf-8'))):
-                print("[-] Pinterest Login Failed [-]\n")
-            else:
+            
+            if (self.driver.title[0] == "("):
                 print("[+] Pinterest Login Success [+]\n")
+            else:
+                if (self.driver.title.split()[1] == "Pinterest"):
+                    print("[+] Pinterest Login Success [+]\n")
+                else:
+                    print("[-] Pinterest Login Failed [-]\n")
         else:
             print(
                 "Pinterest Login Page title field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
